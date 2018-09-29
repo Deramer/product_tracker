50a750d2c5d924dc1e53564dda58f5e7b5c09131
deramer <arseniy.nestyuk@gmail.com>
2018-09-29 19:34:08 +0300

    heroku configs 5

--- product_tracker/settings.py
+++ product_tracker/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'product',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,6 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 django_heroku.settings(locals())
